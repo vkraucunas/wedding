80b56e3f52af1d883e4462e8c2efa9f7105e5795
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-04-09 10:47:38 -0600

    basic routes laid down

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.render('index', {title: "Home"})
+})
+
 router.get('/about', (req, res, next) => {
   res.render('about', {title: "About Us"})
+})
+
+router.get('/event', (req, res, next) => {
+  res.render('event', {title: "Event Details"})
+})
+
+router.get('/rsvp', (req, res, next) => {
+  res.render('rsvp', {title: "RSVP"})
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('login', {title: "login"})
+})
+
+router.post('/login', (req, res, next) => {
+  res.render('login', {title: "login"})
+})
+
+router.get('/logout', (req, res, next) => {
+  res.redirect('/')
 })
 
 module.exports = router;
