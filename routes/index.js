f829151b83085d405d5729d512fc519467c5963a
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-04-09 10:22:40 -0600

    start of about page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/about', (req, res, next) => {
+  res.render('about', {title: "About Us"})
+})
 
 module.exports = router;
